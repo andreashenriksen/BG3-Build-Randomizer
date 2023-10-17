5ed9145481cf921936aafcca65a8d5a1114bd37c
farhadfarhadii <29977968+farhadfarhadii@users.noreply.github.com>
2023-10-17 14:06:50 +0100

    refactor: fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,116 @@
-import { useState } from 'react'
-import { BuildRoulette } from './BuildRoulette'
-import './App.css'
+import { useState } from "react";
+import { BuildRoulette } from "./BuildRoulette";
+import "./App.css";
 
 function App() {
-  const [selectedClasses, setSelectedClasses] = useState<string[]>([])
-  const [selectedClassIcons, setSelectedClassIcons] = useState<string[]>([])
-  const [selectedRace, setSelectedRace] = useState('')
-  const [selectedRacePicture, setSelectedRacePicture] = useState('')
-  const [numClasses, setNumClasses] = useState(1)
+  const [selectedClasses, setSelectedClasses] = useState<string[]>([]);
+  const [selectedClassIcons, setSelectedClassIcons] = useState<string[]>([]);
+  const [selectedRace, setSelectedRace] = useState("");
+  const [selectedRacePicture, setSelectedRacePicture] = useState("");
+  const [numClasses, setNumClasses] = useState(1);
 
   function Dropdown({ numClasses }: { numClasses: number }) {
     const [selectedValue, setSelectedValue] = useState(numClasses.toString());
-  
-    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+
+    const handleSelectChange = (
+      event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
       setSelectedValue(event.target.value);
       setNumClasses(parseInt(event.target.value));
     };
-  
+
     return (
       <select
-        className="bg-gray-800 border-gray-700 rounded-md shadow-sm hover:ring-gray-700 focus:ring focus:ring-gray-700 focus:ring-opacity-50 py-1 px-2" 
+        className="bg-gray-800 border-gray-700 rounded-md shadow-sm hover:ring-gray-700 focus:ring focus:ring-gray-700 focus:ring-opacity-50 py-1 px-2"
         onChange={handleSelectChange}
         style={{ margin: "10px" }}
         value={selectedValue}
       >
-        {Array.from({ length: 12 }, (_, i) => i + 1).map((value) => ( // Create an array of numbers from 1 to 12 and map them to HTML options
-          <option key={value} value={value.toString()}>
-            {value}
-          </option>
-        ))}
+        {Array.from({ length: 12 }, (_, i) => i + 1).map(
+          (
+            value // Create an array of numbers from 1 to 12 and map them to HTML options
+          ) => (
+            <option key={value} value={value.toString()}>
+              {value}
+            </option>
+          )
+        )}
       </select>
     );
   }
 
   function handleClassChange(buildClass: string[]) {
-    setSelectedClasses(buildClass)
+    setSelectedClasses(buildClass);
   }
 
   function handleClassIconChange(buildClassIcon: string[]) {
-    setSelectedClassIcons(buildClassIcon)
+    setSelectedClassIcons(buildClassIcon);
   }
 
   function handleRaceChange(buildRace: string) {
-    setSelectedRace(buildRace)
+    setSelectedRace(buildRace);
   }
 
   function handleRacePictureChange(buildRacePicture: string) {
-    setSelectedRacePicture(buildRacePicture)
+    setSelectedRacePicture(buildRacePicture);
   }
 
   function handleBuild() {
-    const buildRoulette = new BuildRoulette(numClasses)
-    buildRoulette.roll()
-    const classes: string[] = buildRoulette.getSelectedClasses()
-    const classIcons: string[] = buildRoulette.getClassPictures()
-    const race: string = buildRoulette.getRace()
-    const racePicture: string = buildRoulette.getRacePicture()
-    handleClassChange(classes)
-    handleClassIconChange(classIcons)
-    handleRaceChange(race)
-    handleRacePictureChange(racePicture)
+    const buildRoulette = new BuildRoulette(numClasses);
+    buildRoulette.roll();
+    const classes: string[] = buildRoulette.getSelectedClasses();
+    const classIcons: string[] = buildRoulette.getClassPictures();
+    const race: string = buildRoulette.getRace();
+    const racePicture: string = buildRoulette.getRacePicture();
+    handleClassChange(classes);
+    handleClassIconChange(classIcons);
+    handleRaceChange(race);
+    handleRacePictureChange(racePicture);
   }
-  
+
   return (
     <div>
       <div className="fixed top-0 w-full text-center bg-gradient-to-b from-black via-black text-white p-4 text-2xl font-custom">
         Baldur's Gate 3 Build Randomizer
       </div>
       <div className="flex justify-center items-center min-h-screen">
-        <div className="flex-grow bg-gray-800 p-4 rounded-lg shadow-lg" style={{ maxWidth: '40rem', maxHeight: '800px', height: '750px'}}>
+        <div
+          className="flex-grow bg-gray-800 p-4 rounded-lg shadow-lg"
+          style={{ maxWidth: "40rem", maxHeight: "800px", height: "750px" }}
+        >
           <h1 className="text-2xl font-bold text-center">Build</h1>
           <div className="mt-4">
-            <div className="mb-4 w-max justify-center" style={{ height: '100px'}}>
-              <label className="block text-xl font-bold text-gray-300">Race:</label>
-              <img src={selectedRacePicture} style={{ height: '100px', width: '100px', position: 'fixed', left: '735px', top: '150px'}}/>
-              <p className="text-4xl text-gray-200" style={{ height: '22px', position: 'fixed', left: '855px', top: '185px'}}>{selectedRace}</p>
+            <div className="mb-4">
+              <label className="block text-xl font-bold text-gray-300">
+                Race:
+              </label>
+              <div className="flex items-center w-max gap-2 ml-16">
+                <img
+                  src={selectedRacePicture}
+                  className="h-[100px] w-[100px]"
+                />
+                <p className="text-4xl text-gray-200">{selectedRace}</p>
+              </div>
             </div>
-            <div className="mb-4" style={{ height: '364px'}}>
-              <label className="block text-xl font-bold text-gray-300">Classes:</label>
+            <div className="mb-4" style={{ height: "364px" }}>
+              <label className="block text-xl font-bold text-gray-300">
+                Classes:
+              </label>
               <ul>
                 {selectedClasses.map((buildClass, index) => (
-                  <li key={index} className={`text-lg text-gray-200 list-image-[url(${selectedClassIcons[index]})]`}>{buildClass}</li>
+                  <li
+                    key={index}
+                    className={`text-lg text-gray-200 list-image-[url(${selectedClassIcons[index]})]`}
+                  >
+                    {buildClass}
+                  </li>
                 ))}
               </ul>
             </div>
             <div className="mb-4">
-              <label className="block text-sm font-semibold text-gray-300">Number of Classes:</label>
+              <label className="block text-sm font-semibold text-gray-300">
+                Number of Classes:
+              </label>
               <Dropdown numClasses={numClasses} />
             </div>
             <div className="mt-6">
@@ -100,7 +125,7 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
